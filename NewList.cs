56de5c17ab237d83c2d31e6b7eaf99f99b192871
Jen <jnnfrlocke@gmail.com>
2017-09-25 15:20:50 -0500

    Start with new class/file due to messy old code

---
+++ NewList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CustomList
+{
+    public class NewList : IEnumerable
+    {
+        //Iterator
+        public CustomList<int> newList = new CustomList<int>();
+
+        public IEnumerator GetEnumerator()
+        {
+                for (int index = 0; index < newList.Length; index++)
+                {
+                    yield return newList[index];
+                }
+        }
+
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    throw new NotImplementedException();
+        //}
+        //public CustomList<int> IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    for (int index = 0; index < newList.Length; index++)
+        //    {
+        //        yield return newList[index];
+        //        yield return newList[index];
+        //    }
+
+       
+            
+    }
+}
